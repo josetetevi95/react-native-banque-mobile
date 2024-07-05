c5983165e1e44114fd64b2c399b1ebf6832ff680
josetetevi95 <jose.tetevi@gmail.com>
2024-07-05 14:51:17 +0200

    [ADD - UPDATE] : Project base - LoginScreen

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import LoginScreen from './components/LoginScreen'
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="login" component={LoginScreen} />
+        {/* <Drawer.Screen name="add" component={AddScreen} /> */}
+        {/* <Drawer.Screen name="Update" component={UpdateScreen} /> */}
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ components/Test.js
@@ -0,0 +1,4 @@
+import { API_URL, API_KEY } from '@env';
+
+console.log(API_URL);  // Outputs: https://api.example.com
+console.log(API_KEY);  // Outputs: your_api_key_here
